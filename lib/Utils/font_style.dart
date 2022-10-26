c8caa1fc8cf56a5118c4278e59d7319cf5b1f36a
PranoySKumar <pranoyskumar@gmail.com>
2022-10-26 16:43:48 +0530

    renamed default font size variable

--- lib/Utils/font_style.dart
+++ lib/Utils/font_style.dart
@@ -1,39 +1,37 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-const FONT_SIZE_DEFAULT = 16.0;
+const double fontSizeDefault = 16.0;
 
 const interLight = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w300,
-  fontSize: FONT_SIZE_DEFAULT,
+  fontSize: fontSizeDefault,
 );
 const interRegular = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w400,
-  fontSize: FONT_SIZE_DEFAULT,
+  fontSize: fontSizeDefault,
 );
 
 const interMedium = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w500,
-  fontSize: FONT_SIZE_DEFAULT,
+  fontSize: fontSizeDefault,
 );
 
 const interBold = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w700,
-  fontSize: FONT_SIZE_DEFAULT,
+  fontSize: fontSizeDefault,
 );
 const interSemiBold = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w600,
-  fontSize: FONT_SIZE_DEFAULT,
+  fontSize: fontSizeDefault,
 );
 
 const interBlack = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w900,
-  fontSize: FONT_SIZE_DEFAULT,
+  fontSize: fontSizeDefault,
 );
